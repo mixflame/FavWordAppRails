14d7c7faff3cf14f3319b0d2029d91f64d220f40
Jonathan "jsilver1er" Silverman <jsilverone@me.com>
2013-03-31 12:28:52 -0600

    FavWordApp first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FavWordApp::Application.config.secret_token = '49d0ccb0117c3b7ffa53974d2df0155fb1dd49de335af11f7221d91dc8b1a4aa5454619170710c2bf417d6fb2e12ef546829fd3c733abf35e5fc1cdfe47ae969'
